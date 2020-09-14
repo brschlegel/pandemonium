4ba79af63f10b7c80d7866a0198ef0f80d1f4ff8
Ben <brschlegels@gmail.com>
2020-09-13 22:16:42 -0400

    started prop system

---
+++ Pandemonium/Assets/Scripts/Prop/CustomGravity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+public class CustomGravity : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float gravity;
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        rb.AddForce(0,-gravity, 0);
+    }
+}
