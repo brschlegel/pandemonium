0a1bdff446ac3eadd6175e140b45578b63c77dd5
Ben <brschlegels@gmail.com>
2020-09-17 11:51:07 -0400

    Started goal programming

---
+++ Pandemonium/Assets/Scripts/Prop/Goal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class ScoredEvent : UnityEvent<GameObject>
+{
+    
+}
+
+public class Goal : MonoBehaviour
+{
+
+    public ScoredEvent entered;
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "MainCollider")
+        {
+            entered.Invoke(other.gameObject);
+        }
+    }
+
+}
--- Pandemonium/Assets/Scripts/Prop/Spawner.cs
+++ Pandemonium/Assets/Scripts/Prop/Spawner.cs
@@ -8,28 +8,45 @@ public class Spawner : MonoBehaviour
     public GameObject spawnUnder;
     public Vector3 spawnOffset;
     public float spawnForce;
-    public Vector3 spawnDirection; 
+    public Vector3 spawnDirection;
     public bool useRandomDirection;
     public Vector3 spawnRotation;
+    public int numSpawned;
 
-    
+    public float timedInterval;
+    public float startDelay;
+
+
+
+
     void Start()
     {
+        if(timedInterval != 0)
+        {
+            InvokeRepeating("Spawn", startDelay, timedInterval);
+        }
         Spawn();
+
     }
 
     void Update()
     {
-        
+
     }
     public void Spawn()
     {
-        if(useRandomDirection)
+
+        for (int i = 0; i < numSpawned; i++)
         {
-            spawnDirection = Random.insideUnitSphere;
+            if (useRandomDirection)
+            {
+                spawnDirection = Util.GetRandomUnitVector(0, 2 * Mathf.PI, 0, 2 * Mathf.PI);
+            }
+
+            GameObject g = Instantiate(toBeSpawned, transform.position + spawnOffset, Quaternion.Euler(transform.eulerAngles + spawnRotation), spawnUnder.transform);
+            g.GetComponent<Rigidbody>().AddForce(spawnForce * spawnDirection);
         }
-        GameObject g = Instantiate(toBeSpawned,transform.position + spawnOffset, Quaternion.Euler(transform.eulerAngles + spawnRotation), spawnUnder.transform );
-        g.GetComponent<Rigidbody>().AddForce(spawnForce * spawnDirection);
+
     }
 }
 
