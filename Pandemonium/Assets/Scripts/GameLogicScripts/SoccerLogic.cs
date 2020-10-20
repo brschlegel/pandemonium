d8e046f2c376a2fa3987798d78de3f6e216825cf
Ben <brschlegels@gmail.com>
2020-10-20 13:34:02 -0400

    Minigame 1 presentable

--- Pandemonium/Assets/Scripts/GameLogicScripts/SoccerLogic.cs
+++ Pandemonium/Assets/Scripts/GameLogicScripts/SoccerLogic.cs
@@ -6,6 +6,7 @@ public class SoccerLogic : MonoBehaviour
 {
     public GameObject environment;
     public float degreeStep;
+    public float numRotate;
     void Start()
     {
         degreeStep = 1;
@@ -20,6 +21,7 @@ public class SoccerLogic : MonoBehaviour
     }
 
     public void StartRotate(){
+        numRotate = Random.Range(30,200);
         StartCoroutine("Rotate");
     }
 
@@ -28,9 +30,13 @@ public class SoccerLogic : MonoBehaviour
     }
     public IEnumerator Rotate()
     {
-        for(int i =0; i < 100; i++){
+        for(int i =0; i < numRotate; i++){
         environment.transform.Rotate(0,degreeStep,0);
         yield return null;
         }
+    }
+
+    public void SavePlayer(GameObject player){
+        player.transform.position = new Vector3(0,1,0);
     }
 }
