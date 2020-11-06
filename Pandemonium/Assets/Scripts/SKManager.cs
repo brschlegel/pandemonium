0feb7dbb74f248f54946fd89faf1091510d26bdd
JackFu155 <jackfu155@gmail.com>
2020-11-05 20:56:16 -0500

    Added new minigame scene

---
+++ Pandemonium/Assets/Scripts/SKManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SKManager : MonoBehaviour
+{
+    public GameObject meteor;
+    public GameObject platform;
+    public float X;
+    public float Z;
+    private int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        X = platform.transform.localScale.x;
+        Z = platform.transform.localScale.z;
+        count += 1;
+        if(count % 300 == 0)
+        {
+            Instantiate(meteor, new Vector3(Random.Range(-X, X + 1), 100, Random.Range(-Z, Z + 1)), Quaternion.identity);
+        }
+    }
+}
