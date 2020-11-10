d6360b9909bb7c3d94950da15b71d9dc89366ff6
JackFu155 <jackfu155@gmail.com>
2020-11-10 12:27:57 -0500

    Added assets

---
+++ Pandemonium/Assets/Scripts/Explosion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    private int count = 0;
+    public int destructionCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count += 1;
+        if(count == destructionCount)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Pandemonium/Assets/Scripts/Meteor.cs
+++ Pandemonium/Assets/Scripts/Meteor.cs
@@ -6,6 +6,10 @@ public class Meteor : MonoBehaviour
 {
     public GameObject explosion;
     public float destructionHeight;
+    public float rotateX;
+    public float rotateY;
+    public float rotateZ;
+    public bool rotate = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,10 @@ public class Meteor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(rotate == true)
+        {
+            transform.Rotate(rotateX, rotateY, rotateZ);
+        }
         if(transform.position.y <= destructionHeight)
         {
             Explode();
