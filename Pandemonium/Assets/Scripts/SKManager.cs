100b54ae310c257df729030f6a1e50e9c029d530
JackFu155 <jackfu155@gmail.com>
2020-11-17 13:45:09 -0500

    commit

--- Pandemonium/Assets/Scripts/SKManager.cs
+++ Pandemonium/Assets/Scripts/SKManager.cs
@@ -6,8 +6,9 @@ public class SKManager : MonoBehaviour
 {
     public GameObject meteor;
     public GameObject platform;
-    public float X;
-    public float Z;
+    public GameObject crab;
+    //public float X;
+    //public float Z;
     private int count = 0;
     // Start is called before the first frame update
     void Start()
@@ -18,12 +19,16 @@ public class SKManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        X = platform.transform.localScale.x;
-        Z = platform.transform.localScale.z;
+        //X = platform.transform.localScale.x;
+        //Z = platform.transform.localScale.z;
         count += 1;
-        if(count % 300 == 0)
+        if(count % 500 == 0)
         {
-            Instantiate(meteor, new Vector3(Random.Range(-X, X + 1), 100, Random.Range(-Z, Z + 1)), Quaternion.identity);
+            Instantiate(meteor, new Vector3(Random.Range(-30, 68 + 1), 50, Random.Range(-22, 63 + 1)), Quaternion.identity);
+        }
+        if(count % 100 == 0 && count % 75 == 0)
+        {
+            Instantiate(crab, new Vector3(Random.Range(-30, 68 + 1), 0, Random.Range(-22, 63 + 1)), Quaternion.identity);
         }
     }
 }
